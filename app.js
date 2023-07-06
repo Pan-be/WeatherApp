d168dcb826ae6977d69d309f6c42a59db9b8e739
Konrad <konradbieniek@gmail.com>
2023-07-06 22:27:03 +0200

    added icon

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import express from "express"
+import express, { json } from "express"
 import https from "https"
 import "dotenv/config"
 
@@ -8,8 +8,23 @@ const apiKey = process.env.WEATHER_ID
 app.get("/", (req, res) => {
 	const url = `https://api.openweathermap.org/data/2.5/weather?q=Bielsko-Biala&appid=${apiKey}&units=metric`
 
-	https.get(url, (response) => console.log(response.statusCode))
-	res.send("server is running")
+	https.get(url, (response) => {
+		console.log(response.statusCode)
+		response.on("data", (data) => {
+			const weatherData = JSON.parse(data)
+			const temp = weatherData.main.temp
+			const desc = weatherData.weather[0].description
+			const iconNum = weatherData.weather[0].icon
+			const iconURL = `https://openweathermap.org/img/wn/${iconNum}@2x.png`
+			// console.log(iconNum)
+			res.send(
+				`<div><h1>The temperature in Bielsko-Biała is ${temp} degrees Celcius</h1>
+				<p>The weather is currently ${desc}</p>
+				<img src="${iconURL}">
+				</div>`
+			)
+		})
+	})
 })
 
 app.listen(3000, () => console.log("server is running on port 3000."))
