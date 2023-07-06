1106155943d0a17aeeb083078866b827e9e8935e
Konrad <konradbieniek@gmail.com>
2023-07-06 16:56:35 +0200

    added dotenv package and .env file

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 import express from "express"
+import https from "https"
+import "dotenv/config"
 
 const app = express()
+const apiKey = process.env.WEATHER_ID
 
 app.get("/", (req, res) => {
+	const url = `https://api.openweathermap.org/data/2.5/weather?q=Bielsko-Biala&appid=${apiKey}&units=metric`
+
+	https.get(url, (response) => console.log(response.statusCode))
 	res.send("server is running")
 })
 
