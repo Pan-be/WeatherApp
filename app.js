69fbfa72afaa5ddde9983c826fb8ece42477c307
Konrad <konradbieniek@gmail.com>
2023-07-07 23:38:50 +0200

    added __dirname functionality

--- app.js
+++ app.js
@@ -1,30 +1,40 @@
 import express, { json } from "express"
 import https from "https"
 import "dotenv/config"
+import path from "path"
+import { fileURLToPath } from "url"
+const __filename = fileURLToPath(import.meta.url)
+
+const __dirname = path.dirname(__filename)
 
 const app = express()
 const apiKey = process.env.WEATHER_ID
 
 app.get("/", (req, res) => {
-	const url = `https://api.openweathermap.org/data/2.5/weather?q=Bielsko-Biala&appid=${apiKey}&units=metric`
+	res.sendFile(`${__dirname}/index.html`)
 
-	https.get(url, (response) => {
-		console.log(response.statusCode)
-		response.on("data", (data) => {
-			const weatherData = JSON.parse(data)
-			const temp = weatherData.main.temp
-			const desc = weatherData.weather[0].description
-			const iconNum = weatherData.weather[0].icon
-			const iconURL = `https://openweathermap.org/img/wn/${iconNum}@2x.png`
-			// console.log(iconNum)
-			res.send(
-				`<div><h1>The temperature in Bielsko-Biała is ${temp} degrees Celcius</h1>
-				<p>The weather is currently ${desc}</p>
-				<img src="${iconURL}">
-				</div>`
-			)
-		})
-	})
+	// const query = "Bielsko-Biala"
+	// const unit = "metric"
+
+	// const url = `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${apiKey}&units=${unit}`
+
+	// https.get(url, (response) => {
+	// 	console.log(response.statusCode)
+	// 	response.on("data", (data) => {
+	// 		const weatherData = JSON.parse(data)
+	// 		const temp = weatherData.main.temp
+	// 		const desc = weatherData.weather[0].description
+	// 		const iconNum = weatherData.weather[0].icon
+	// 		const iconURL = `https://openweathermap.org/img/wn/${iconNum}@2x.png`
+	// 		// console.log(iconNum)
+	// 		res.send(
+	// 			`<div><h1>The temperature in Bielsko-Biała is ${temp} degrees Celcius</h1>
+	// 			<p>The weather is currently ${desc}</p>
+	// 			<img src="${iconURL}">
+	// 			</div>`
+	// 		)
+	// 	})
+	// })
 })
 
 app.listen(3000, () => console.log("server is running on port 3000."))
